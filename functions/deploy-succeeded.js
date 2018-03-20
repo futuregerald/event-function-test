5cbf6f48e3484c2f507de2f710a768f1c4d5ba98
Gerald O <gerald.onyango@gmail.com>
2018-03-20 15:21:15 -0400

    testing with http.get

--- functions/deploy-succeeded.js
+++ functions/deploy-succeeded.js
@@ -1,6 +1,22 @@
+const https = require('https');
+
 exports.handler = function(event, context, callback) {
   console.log('The deploy succeeded event has been triggered');
   console.log(event);
+  const url = 'https://hookb.in/KAnnGg3N';
+
+  https.get(url, res => {
+    res.setEncoding('utf8');
+    let body = '';
+    res.on('data', data => {
+      body += data;
+    });
+    res.on('end', () => {
+      body = JSON.parse(body);
+      console.log(body);
+    });
+  });
+
   callback(null, {
     statusCode: 200,
     body: JSON.stringify({ Test: 'Yup, testing' }),

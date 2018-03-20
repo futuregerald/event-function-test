61e1c58961d822b3083836dd63fb0ed9bbbaada1
Gerald O <gerald.onyango@gmail.com>
2018-03-20 15:27:47 -0400

    testing

--- functions/deploy-succeeded.js
+++ functions/deploy-succeeded.js
@@ -1,43 +1,6 @@
-var https = require('https');
-
 exports.handler = function(event, context, callback) {
   console.log('The deploy succeeded event has been triggered');
   console.log(event);
-  const url = 'https://hookb.in/KAnnGg3N';
-
-  https.get(url, res => {
-
-    var data = JSON.stringify({
-        name: "John"
-    });
-    
-    var options = {
-        host: "hookb.in",
-        port: 443,
-        path: "/KAnnGg3N",
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Content-Length": Buffer.byteLength(data)
-        }
-    };
-    
-    var req = https.request(options, function (res) {
-        var response = "";
-    
-        res.setEncoding("utf8");
-    
-        res.on("data", function (chunk) {
-            response += chunk;
-        });
-    
-        res.on("end", function () {
-            console.log(response);
-        });
-    });
-    
-    req.write(data);
-    req.end();
   callback(null, {
     statusCode: 200,
     body: JSON.stringify({ Test: 'Yup, testing' }),
